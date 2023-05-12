c63f8f5fa62144e7fb0b354190daa85df10b362b
Liam Huang <liamhuang0205@gmail.com>
2023-05-12 10:02:34 +0800

    [build.sh] fix typo.

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -eux
 PACKAGE_PATH=${1}
 PACKAGE_FULLNAME="$(grep Package: "$PACKAGE_PATH"/DEBIAN/control | cut -f2 -d ' ')"_"$(grep Version: "$PACKAGE_PATH"/DEBIAN/control | cut -f2 -d ' ')"_"$(grep Architecture: "$PACKAGE_PATH"/DEBIAN/control | cut -f2 -d ' ')"
 
-dpkg-deb -b ${PACKAGE_PATH} ${PACKAGE_FULLNAME}
+dpkg-deb -b ${PACKAGE_PATH} ${PACKAGE_FULLNAME}.deb
+echo $(pwd)/${PACKAGE_FULLNAME}.deb
 
